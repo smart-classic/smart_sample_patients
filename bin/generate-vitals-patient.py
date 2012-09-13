fcfa294a6f78cf37885de91f681de6e56baba9c7
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2012-09-13 23:18:13 +0300

    Adding rdfs namespace to the vitals patient generator

--- bin/generate-vitals-patient.py
+++ bin/generate-vitals-patient.py
@@ -159,6 +159,7 @@ def codesToRDF (codes):
 
 header = """<?xml version="1.0" encoding="utf-8"?>
 <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+  xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"
   xmlns:sp="http://smartplatforms.org/terms#"
   xmlns:spcode="http://smartplatforms.org/terms/codes/"
   xmlns:foaf="http://xmlns.com/foaf/0.1/"
